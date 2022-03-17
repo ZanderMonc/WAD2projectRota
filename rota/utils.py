a0822bd7599b2bbe4cebc5c703a3adb991e6438f
Zander Moncrieff <2555900M@student.gla.ac.uk>
2022-03-17 12:26:16 +0000

    Added requirements.txt, added next and previous month buttons

--- rota/utils.py
+++ rota/utils.py
@@ -10,7 +10,7 @@ class Table(HTMLCalendar):
         super(Table, self).__init__()
 
     def formatday(self, day, shifts):
-        shifts_on_day = shifts.filter(request_date__day = day)
+        shifts_on_day = shifts.filter(request_date__day=day)
         d = ''
         for shift in shifts_on_day:
             d += 'SHIFT: '
@@ -20,13 +20,14 @@ class Table(HTMLCalendar):
         return '<td></td>'
 
     def formatweek(self, week, shifts):
+        shifts = Request.objects.filter(request_date__month=self.month)  # gets this month's shifts
         weekstring = ''
         for d, weekday in week:
             weekstring += self.formatday(d, shifts)
         return f'<tr> {weekstring} </tr>'
 
     def formatmonth(self, withyear=True):
-        shifts = Request.objects.filter()
+        shifts = Request.objects.filter(request_date__year=self.year)  # gets only this year's shifts
         tbl = f'<table border="0" cellpadding="0" cellspacing="0" class="timetable">\n'
         tbl += f'{self.formatmonthname(self.year, self.month, withyear=withyear)}\n'
         tbl += f'{self.formatweekheader()}\n'
