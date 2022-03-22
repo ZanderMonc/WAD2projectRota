fa4fe1570c970ca850ea2fbc51faae4f251af883
Zander Moncrieff <2555900M@student.gla.ac.uk>
2022-03-22 00:30:41 +0000

    working prototype google login to site

--- RotaCare/urls.py
+++ RotaCare/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('', include('rota.urls'), name='index'),
     path('rota/', include('rota.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls'), name='google'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
