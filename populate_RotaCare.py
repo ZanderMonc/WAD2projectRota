49f7ce34af6db5734ac5d44e0f7bb47c664d795e
Zander Moncrieff <2555900M@student.gla.ac.uk>
2022-03-24 20:04:51 +0000

    improved test

--- populate_RotaCare.py
+++ populate_RotaCare.py
@@ -21,14 +21,15 @@ def populate():
         # code to create random id, still chance of violating unique username:
         # temp = int(users['registration_id']) + random.randint(0,10000)
         users['registration_id'] = str(i)  # str(temp)
-        add_UserProfile(registration_id=i + 61, user=User.objects.create_user('jane' + str(i), 'doe' + users['registration_id'] + '@nhs.com',
-         'janepassword'))  # for loop adds two
-        # default jane doe users
+        add_UserProfile(registration_id=i, user=User.objects.create_user('jane' + str(i),
+                                                                         'doe' + users['registration_id'] + '@nhs.com',
+                                                                         'janepassword'))
+        # for loop adds ten default jane doe users
 
-        # users['registration_id'] = str(temp)
-
-    add_Timetable()  # adds a default timetable object
-    add_Request()  # adds a default request object
+    add_Timetable()  # adds the default timetable object
+    for i in range(30):
+        add_Request(request_date=datetime.datetime.now() + datetime.timedelta(days=i), )
+        # adds 30 default request objects once daily from the present day.
 
 
 def add_UserProfile(registration_id, user):
@@ -42,18 +43,19 @@ def add_UserProfile(registration_id, user):
 
 
 def add_Request(requested_by_staff="unknown", request_id="0",
-                request_date=datetime.datetime.now(), ):
+                request_date=datetime.datetime.now(), staff_job_title="Staff Nurse", shift_time="night_shift"):
     r, success = Request.objects.get_or_create(requested_by_staff=requested_by_staff,
                                                request_id=request_id,
-                                               staff_job_title="Charge Nurse",
+                                               staff_job_title=staff_job_title,
                                                request_date=request_date,
-                                               shift_time="night_shift")
+                                               shift_time=shift_time)
     r.save()
     return r
 
 
 def add_Timetable():
-    t, success = Timetable.objects.get_or_create(timetable_id="23", staff_name="Jane", registration_id="0", day="monday",
+    t, success = Timetable.objects.get_or_create(timetable_id="23", staff_name="Jane", registration_id="0",
+                                                 day="monday",
                                                  week_name="third",
                                                  month_name="March", date=datetime.datetime.now())
     t.save()
