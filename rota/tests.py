9e6efffc0981dd93beb1b8a8219e7a75b5f6f7f6
Zander Moncrieff <2555900M@student.gla.ac.uk>
2022-03-25 16:58:29 +0000

    functionality changes - charge nurse adding shift + more test work

--- rota/tests.py
+++ rota/tests.py
@@ -16,6 +16,21 @@ FAILURE_HEADER = f"{os.linesep}{os.linesep}{os.linesep}================{os.lines
 FAILURE_FOOTER = f"{os.linesep}"
 
 
+def add_UserProfile(user, registration_id="0", first_name="Jane",
+                    last_name="Doe", job_title="Nurse", phone_number="00000000000", ward="A2",
+                    date_admission=datetime.now(), image=""):
+    user.save()
+    u, success = UserProfile.objects.get_or_create(user=user,
+                                                   registration_id=registration_id,
+                                                   first_name=first_name,
+                                                   last_name=last_name,
+                                                   job_title=job_title,
+                                                   phone_number=phone_number,
+                                                   ward=ward,
+                                                   date_admission=date_admission,
+                                                   image=image)
+    u.save()
+    return u
 class ConfigTestCase(TestCase):
 
     def test_middleware_present(self):
@@ -30,45 +45,35 @@ class ConfigTestCase(TestCase):
 
 class ModelsTestCase(TestCase):
     def setUp(self):
-        Request.objects.create(requested_by_staff="unknown", request_id="0", request_type="unknown",
-                               request_date="2001-01-01 00:01",
-                               date_requesting="2022-09-09 00:05", swap_staff="Jane Doe")
-        Request.objects.create(requested_by_staff="JaneDoe", request_id="1", request_type="unknown",
-                               request_date="2001-01-01 00:01",
-                               date_requesting="2022-09-09 00:05", swap_staff="Jane Doe")
-
+        Request.objects.create(requested_by_staff="unknown", request_id="0",
+                                      request_date="2001-01-01 00:01", )
+        Request.objects.create(requested_by_staff="JaneDoe", request_id="1",
+                                      request_date="2001-01-01 00:01")
         user1 = User.objects.create_user('jane',
                                          'doe' + '@nhs.com',
                                          'janepassword')
         user2 = User.objects.create_user('joe',
                                          'done@nhs.com',
                                          'joepassword')
-        UserProfile.objects.create(user1, registration_id="0", first_name="Jane",
-                                   last_name="Doe", job_title="Charge Nurse",
-                                   phone_number="00000000000", ward="A2",
-                                   date_admission=datetime.datetime.now(), image="")
-        UserProfile.objects.create(user2, registration_id="1", first_name="Joe",
-                                   last_name="Done", job_title="Healthcare Support Assistant",
-                                   phone_number="00000000001", ward="A1",
-                                   date_admission=datetime.datetime.now(), image="")
 
-    def test_requests_are_correct(self):
-        Req1 = Request.objects.get(requested_by_staff="unknown")
-        Req2 = Request.objects.get(requested_by_staff="JaneDoe")
-        self.assertEqual(Req1.request_id, 0)
-        self.assertEqual(Req2.request_id, 1)
+        add_UserProfile(user1, registration_id="0", first_name="Jane",
+                                          last_name="Doe", job_title="Charge Nurse",
+                                          phone_number="00000000000", ward="A2",
+                                          date_admission=datetime.now(), image="")
+        add_UserProfile(user2, registration_id="1", first_name="Joe",
+                                          last_name="Done", job_title="Healthcare Support Assistant",
+                                          phone_number="00000000001", ward="A1",
+                                          date_admission=datetime.now(), image="")
 
-    def test_UserProfiles_are_correct(self):
-        Usr1 = UserProfile.objects.get(registration_id="0")
-        Usr2 = UserProfile.objects.get(registration_id="1")
-        self.assertEqual(Usr1.job_title, "Charge Nurse")
-        self.assertEqual(Usr2.job_title, "Healthcare Support Assistant")
+    def test_request_contents(self):
+        self.assertEqual("JaneDoe", Request.objects.get(request_id="1").requested_by_staff)
 
-
+    def test_userprofile_contents(self):
+        self.assertNotEqual("Jane", UserProfile.objects.get(registration_id="1").first_name)
+        self.assertEqual("Jane", UserProfile.objects.get(registration_id="0").first_name)
 class ViewTests(TestCase):
 
     def test_index_view(self):
         response = self.client.get(reverse('rota:index'))
         content = response.content.decode()
         self.assertTrue(content)
-
