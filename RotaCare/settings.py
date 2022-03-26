b70153215a94244870cd8dca48d4e464e567f498
Zander Moncrieff <2555900M@student.gla.ac.uk>
2022-03-26 23:48:59 +0000

    fixes for python anywhere upload

--- RotaCare/settings.py
+++ RotaCare/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'nu$f^a+8@e(y6l(^jjnktf24l)06zi2g*dd8qe4c71agxdp5kt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -117,11 +117,9 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR, ]
 
